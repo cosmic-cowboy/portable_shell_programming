9207407f9d247d4d743f136188f38bdd855aa79b
hiroo takaku <slgerkamp@gmail.com>
2014-09-15 17:24:57 +0900

    8 シェルのいろいろな機能：整数の計算

---
+++ Chapter08_Shell_Utilities/expr.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# 整数の計算
+echo `expr 3 + 5`
+echo `expr 3 - 5`
+echo `expr -4 + -2`
+echo `expr 3 * 5`
+echo `expr 3 \* 5`
+echo `expr 3 '*' 5`
+echo `expr 3 / 5`
+echo `expr 3 / -3`
+echo `expr 30 % 5`
+echo `expr 3 % 5`
+echo `expr -20 % 3`
